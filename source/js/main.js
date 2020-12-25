c282ad0f54f9deea6a014e446c739a33ec08a8ae
yulin-liang <yliangsysu@gmail.com>
2020-12-24 20:36:45 -0800

    make the toc sticky

--- source/js/main.js
+++ source/js/main.js
@@ -21,7 +21,6 @@ $('#back-to-top').on('click', function() {
     $('html, body').animate({ scrollTop: 0 }, 200);
 });
 
-
 function registerPostTOC() {
     const navItems = document.querySelectorAll('.post-toc li');
     const sections = [...navItems].map(element => {
